0410da8c890c8b02c3df3a8ec5c041f209eb14da
ALI <ali.mughal.1895@gmail.com>
2020-08-30 20:49:53 +0500

    uploading dist folder

---
+++ dist/js/about.7ccef2b0.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},i=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",[n("h1"),n("p",[t._v("This is To Do List App. It is the sample task")])])}],a=n("2877"),l={},r=Object(a["a"])(l,s,i,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=about.7ccef2b0.js.map
